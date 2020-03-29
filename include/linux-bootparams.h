db8db97f3b7650fb47879cab1bbdd73013dc2075
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-03-29 20:16:58 +0200

    defs: use new kernel info structure
    Linux boot protocol versions 2.15+ use separate section to provide static
    information about the kernel, see [1]. MLE header offset, previously found
    directly in zero_page, is moved to this structure. This patch searches for
    proper address, located in new kernel_info structure.
    
    [1] https://01.org/linuxgraphics/gfx-docs/drm/x86/boot.html
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- include/linux-bootparams.h
+++ include/linux-bootparams.h
@@ -7,14 +7,26 @@ struct boot_params {
     u32 tb_dev_map;
     u8 _pad2[0x118];
     u32 syssize;
-    u8 _pad4[0x01c];
+    u8 _pad3[0x00e];
+    u16 version;
+    u8 _pad4[0x00c];
     u32 code32_start;
     u8 _pad6[0x010];
     u32 cmd_line_ptr;
     u8 _pad8[0x00c];
     u32 cmdline_size;
     u8 _pad10[0x02c];
-    u32 mle_header;
+    u32 kern_info_offset;
+};
+
+#define KERNEL_INFO_HEADER 0x506f544c
+
+struct kernel_info {
+    u32 header;
+    u32 size;
+    u32 size_total;
+    u32 setup_type_max;
+    u32 mle_header_offset;
 };
 
 #endif /* _LINUX_BOOTPARAMS_H */
