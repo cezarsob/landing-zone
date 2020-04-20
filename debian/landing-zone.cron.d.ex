8302a2b94c42a65b194c7682c2dbfb63a468b041
Cezary Sobczak <cezary.sobczak@3mdeb.com>
2020-04-20 16:04:58 +0200

    debian/: modify templates and delete redundant files
    Signed-off-by: Cezary Sobczak <cezary.sobczak@3mdeb.com>

--- debian/landing-zone.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the landing-zone package
-#
-0 4	* * *	root	[ -x /usr/bin/landing-zone_maintenance ] && /usr/bin/landing-zone_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(landing-zone):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="landing-zone" command="/usr/bin/landing-zone"
